1dbb33b37cd190a271375d1f8e67a4ebeaf049e7
BrandonMcAlonan <brandon.mcalonan@my.jcu.edu.au>
2020-10-08 15:27:36 +1000

    add basic layout for calculator activity

---
+++ app/src/main/java/com/example/housingapp/calculator.java
@@ -0,0 +1,16 @@
+package com.example.housingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+
+}
