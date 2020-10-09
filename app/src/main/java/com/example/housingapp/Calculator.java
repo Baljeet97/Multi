8c094a32e604f4b17d4950dc6418792f1002c398
Baljeet <baljeetbunny97@gmail.com>
2020-10-09 15:07:53 +1000

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	app/src/main/res/layout/activity_calculator.xml

--- app/src/main/java/com/example/housingapp/Calculator.java
+++ app/src/main/java/com/example/housingapp/Calculator.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class calculator extends AppCompatActivity {
+public class Calculator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
