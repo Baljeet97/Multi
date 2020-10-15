61090269c6b012cf8eed6e163da66bab6bbfe12f
BrandonMcAlonan <brandon.mcalonan@my.jcu.edu.au>
2020-10-15 21:31:56 +1000

    Add links to rental properties

--- app/src/main/java/com/example/housingapp/FindRoom.java
+++ app/src/main/java/com/example/housingapp/FindRoom.java
@@ -2,13 +2,89 @@ package com.example.housingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 
 public class FindRoom extends AppCompatActivity {
+    Button domainLow, domainMed, domainHigh, realEstateLow, realEstateMed, realEstateHigh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_room);
+
+        domainLow = findViewById(R.id.domainLow);
+        domainMed = findViewById(R.id.domainMed);
+        domainHigh = findViewById(R.id.domainHigh);
+        realEstateLow = findViewById(R.id.realEstateLow);
+        realEstateMed = findViewById(R.id.realEstateMed);
+        realEstateHigh = findViewById(R.id.realEstateHigh);
+
+        domainLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.domain.com.au/rent/?suburb=smithfield-qld-4878,trinity-" +
+                        "beach-qld-4879,trinity-park-qld-4879,manunda-qld-4870,manoora-qld-4870," +
+                        "edge-hill-qld-4870&price=100-150");
+            }
+        });
+
+        domainMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.domain.com.au/rent/?suburb=smithfield-qld-4878,trinity-" +
+                        "beach-qld-4879,trinity-park-qld-4879,manunda-qld-4870,manoora-qld-4870," +
+                        "edge-hill-qld-4870&price=150-250");
+            }
+        });
+
+        domainHigh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.domain.com.au/rent/?suburb=smithfield-qld-4878,trinity-" +
+                        "beach-qld-4879,trinity-park-qld-4879,manunda-qld-4870,manoora-qld-4870," +
+                        "edge-hill-qld-4870&price=250-600");
+            }
+        });
+
+        realEstateLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.realestate.com.au/rent/between-50-150-in-manunda,+qld+" +
+                        "4870%3b+manoora,+qld+4870%3b+cairns+north,+qld+4870%3b+cairns+city,+" +
+                        "qld+4870%3b+trinity+beach,+qld+4879%3b+trinity+park,+qld+4879%3b+kewarra" +
+                        "+beach,+qld+4879/list-1?activeSort=price-asc&source=refinement");
+            }
+        });
+
+        realEstateMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.realestate.com.au/rent/between-150-250-in-manunda,+qld+" +
+                        "4870%3b+manoora,+qld+4870%3b+cairns+north,+qld+4870%3b+cairns+city,+" +
+                        "qld+4870%3b+trinity+beach,+qld+4879%3b+trinity+park,+qld+4879%3b+kewarra" +
+                        "+beach,+qld+4879/list-1?activeSort=price-asc&source=refinement");
+            }
+        });
+
+        realEstateHigh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.realestate.com.au/rent/between-250-500-in-manunda,+qld+" +
+                        "4870%3b+manoora,+qld+4870%3b+cairns+north,+qld+4870%3b+cairns+city,+" +
+                        "qld+4870%3b+trinity+beach,+qld+4879%3b+trinity+park,+qld+4879%3b+kewarra" +
+                        "+beach,+qld+4879/list-1?activeSort=price-asc&source=refinement");
+            }
+        });
+
+
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
